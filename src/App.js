16cd45f7fc836be8d6bd0592edfd2cb62b8e0da9
Christian Townsdin <ctownsdin@expedia.org>
2018-04-04 15:59:11 -0700

    add files

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import './App.css'
+
+// MUICSS
+import Tabs from 'muicss/lib/react/tabs'
+import Tab from 'muicss/lib/react/tab'
+
+// TABLES
+import PokemonApp from './tables/pokemon-table/PokemonApp'
+
+class MuiTabs extends React.Component {
+  onChange (i, value, tab, ev) {
+    // console.log(arguments)
+  }
+
+  onActive (tab) {
+    // console.log(arguments)
+  }
+
+  render () {
+    return (
+      <div>
+        <Tabs onChange={this.onChange} defaultSelectedIndex={0}>
+          <Tab value='pane-1' label='Tab 1' onActive={this.onActive}>
+            <PokemonApp />
+          </Tab>
+          <Tab value='pane-2' label='Tab 2'>
+            <SayHi />
+          </Tab>
+          <Tab value='pane-2' label='Tab 2'>
+            <h1>That's all folks!</h1>
+          </Tab>
+        </Tabs>
+      </div>
+    )
+  }
+}
+
+class SayHi extends React.Component {
+  render () {
+    return (
+      <h1>Hi</h1>
+    )
+  }
+}
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <MuiTabs />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/tables/pokemon-table/PokemonForm.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+
+class PokemonForm extends Component {
+  constructor (props) {
+    super(props)
+    const { monToEdit } = this.props
+    if (monToEdit) {
+      this.state = {
+        number: monToEdit.number,
+        name: monToEdit.name,
+        type: monToEdit.type,
+        image: monToEdit.image
+      }
+    } else {
+      this.state = {
+        number: '',
+        name: '',
+        type: '',
+        image: ''
+      }
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleInputChange (event) {
+    const target = event.target
+    const value = target.type === 'checkbox' ? target.checked : target.value
+    const name = target.name
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit (event) {
+    event.preventDefault()
+    this.props.addPokemon(this.state)
+  }
+
+  render () {
+    return (
+      <form onSubmit={this.handleSubmit} className='pokemon-background'>
+        <div>
+          <label>
+            Number:<input type='text' value={this.state.number} name='number' onChange={this.handleInputChange} />
+          </label>
+        </div>
+        <div>
+          <label>
+            Name:<input type='text' value={this.state.name} name='name' onChange={this.handleInputChange} />
+          </label>
+        </div>
+        <div>
+          <label>
+            Type:<input type='text' value={this.state.type} name='type' onChange={this.handleInputChange} />
+          </label>
+        </div>
+        <div>
+          <label>
+            Image:<input type='text' value={this.state.image} name='image' onChange={this.handleInputChange} />
+          </label>
+        </div>
+        <input type='submit' value='Submit' />
+      </form>
+    )
+  }
+}
+
+export default PokemonForm
